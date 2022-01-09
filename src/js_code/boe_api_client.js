290c67d7a5964374678306494809665e8d39a532
byt3-m3 <cbaxtertech@gmail.com>
2022-01-09 15:32:50 -0500

    made function calls async

--- src/js_code/boe_api_client.js
+++ src/js_code/boe_api_client.js
@@ -1,92 +1,51 @@
-export class BoeApiClient {
+export async function invokeCreateFamilyLocalUser(
+    family_name,
+    first_name,
+    last_name,
+    desired_username,
+    password,
+    dob,
+    email
+) {
 
-    constructor(url) {
-        this.url = url
-        this.user_manager_endpoint = '/user_manager'
-        this.axiosClient = require('axios').default;
-        // this.axiosClient.defaults.withCredentials = true
+
+    let axiosClient = require('axios').default;
+    let url = 'http://192.168.1.5:5001/api/v1/registration/family/local'
+    let event = {
+        "FamilyRegistrationLocalRequest": {
+            family_name: family_name,
+            first_name: first_name,
+            last_name: last_name,
+            desired_username: desired_username,
+            password_hash: password,
+            dob: dob,
+            email: email,
+        }
     }
 
-    createNewFamily(
-        family_name,
-        first_name,
-        last_name,
-        email,
-        dob,
-    ) {
-        let url = `${this.url}${this.user_manager_endpoint}/family`
-        console.log(url)
-        this.axiosClient.post(
-            url,
-            {
-                NewFamilyEvent: {
-                    description: family_name,
-                    name: family_name,
-                    first_name: first_name,
-                    last_name: last_name,
-                    email: email,
-                    dob: dob,
-                    subscription_type: 0
-                }
-            },
-        ).then((response) => {
-            console.log(
-                `New Family Requested, FamilyID=${response.headers['x-family-id']}`
-            )
-            alert(`New Family Requested, FamilyID=${response.headers['x-family-id']}`)
-            console.log(response.headers)
-
-
-        }).catch((error) => {
-            if (error.response) {
-                alert(`Unable to Access ${error.response.url}`)
-
-            } else {
-                console.log(`Unable to Access: ${this.url}`)
-
-            }
-
-        })
-    }
-}
-
-
-export function getBoeApiClient() {
-    return new BoeApiClient(
-        "http://127.0.0.1:5000/api/v1"
+    return axiosClient.post(
+        url,
+        event
     )
-
 }
 
-export function authenticateUser(
+
+export async function authenticateUser(
     username,
     password
 ) {
     let axiosClient = require('axios').default;
-    let auth_api_endpoint = 'http://192.168.1.5:9000/api/v1/auth/basic'
+    let auth_api_endpoint = 'http://192.168.1.5:9000/api/v1/auth/local/basic'
 
     return axiosClient.post(
         auth_api_endpoint,
         {
-            "UserAuthRequestEvent": {
+            "LocalAuthRequest": {
                 "username": username,
                 "password": password
             }
         }
-    ).then((response) => {
-
-        return response
+    )
 
 
-    }).catch((error) => {
-        if (error.response) {
-            console.log(`Error Encountered ${error}`)
-            return error
-
-        } else {
-            console.log(`Unable to Access: ${auth_api_endpoint}`)
-
-        }
-
-    })
 }
