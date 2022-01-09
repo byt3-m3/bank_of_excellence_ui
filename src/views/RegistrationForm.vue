b2c071e06175c3acac96c40f1d3e90b4c7c0a65f
byt3-m3 <cbaxtertech@gmail.com>
2022-01-09 15:32:05 -0500

    Added data implemented registration call to backend

--- src/views/RegistrationForm.vue
+++ src/views/RegistrationForm.vue
@@ -13,8 +13,12 @@
           <label for="text-last_name">Last Name</label>
           <b-form-input v-model="last_name" type="text" id="text-last_name"></b-form-input>
 
-          <label for="text-desired_username">Desired Username</label>
-          <b-form-input v-model="desired_username" type="text" id="text-desired_username"></b-form-input>
+
+          <label for="text-desired_username" >Desired Username</label>
+          <b-form-input v-model="desired_username" :placeholder="desired_username" type="text" id="text-desired_username"></b-form-input>
+
+          <label for="text-password">Password</label>
+          <b-form-input v-model="password" type="password" id="text-password"></b-form-input>
 
           <label for="text-dob">Date of Birth</label>
           <b-form-input v-model="dob" type="date" id="text-dob"></b-form-input>
@@ -22,14 +26,13 @@
           <label for="text-email">Email</label>
           <b-form-input v-model="email" type="email" id="text-email"></b-form-input>
 
-          <label for="text-password">Password</label>
-          <b-form-input v-model="password" type="password" id="text-password"></b-form-input>
+
           <b-form-group>
 
           </b-form-group>
         </b-form>
 
-        <b-button @click="submitFamilyRegistration" variant="primary">Submit Registration</b-button>
+        <b-button @click="submitFamilyRegistration()" variant="primary">Submit Registration</b-button>
         <!--        <b-button @click="navigateToRegistrationPage" variant="primary">Register</b-button>-->
 
 
@@ -42,25 +45,43 @@
 </template>
 
 <script>
+import {invokeCreateFamilyLocalUser} from "../js_code/boe_api_client";
+
 export default {
   name: "RegistrationForm",
   data() {
     return {
       family_name: '',
-      family_description: '',
       first_name: '',
       last_name: '',
       desired_username: '',
       password: '',
-      subscription_type: null,
       email: null,
       dob: null,
 
     }
   },
   methods: {
-    submitFamilyRegistration: function () {
-      alert("Received Registration Request")
+    submitFamilyRegistration: async function () {
+
+
+      let response = await invokeCreateFamilyLocalUser(
+          this.$data.family_name,
+          this.$data.first_name,
+          this.$data.last_name,
+          this.$data.desired_username,
+          this.$data.password,
+          this.$data.dob,
+          this.$data.email
+      )
+      if (response.status == 200) {
+        alert("Registration Successful")
+
+        this.$router.push("/login")
+      }
+      console.log(response)
+
+
     }
   }
 }
